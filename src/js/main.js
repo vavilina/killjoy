6a8db48e45ba67060311181fccc01bb2be447fa8
vavilina <ann@myplaycity.com>
2016-10-18 19:25:13 +0400

    test

--- src/js/main.js
+++ src/js/main.js
@@ -9,8 +9,86 @@
  */
 //= partials/app.js
 
-function filter() {
-	$(".button-filter").click(function() {
-		$("#picture").attr('filter', $(this).attr("id"));
+function form() {
+
+	var x = $('#mainForms .form-login');
+		console.log(x);
+		inputs(x);
+	$("#mainLogin").click(function() {
+		$("#modalLogin").toggleClass('_hidden');
+		x = $('#Forms .form-login');
+	});
+	$("#modalClose").click(function() {
+		$("#modalLogin").toggleClass('_hidden');
+		x = $('#mainForms .form-login');
+	});
+	
+
+	$("#buttonLogin").click(function() {
+		$("#Forms").attr('state', 'isLogin');
+		x = $('#Forms .form-login');
+		inputs(x);
+		console.log(x);
+	});
+	$("#buttonRemembered").click(function() {
+		$("#Forms").attr('state', 'isLogin');
+		x = $('#Forms .form-login');
+		inputs(x);
+	});
+	$("#buttonSignup").click(function() {
+		$("#Forms").attr('state', 'isSignup');
+		x = $('#Forms .form-signup');
+		inputs(x);
+	});
+	$("#buttonRecover").click(function() {
+		$("#Forms").attr('state', 'isRecover');
+		x = $('#Forms .form-recover');
+		inputs(x);
+	});
+
+	$("#mainbuttonLogin").click(function() {
+		$("#mainForms").attr('state', 'isLogin');
+		x = $('#mainForms .form-login');
+		inputs(x);
+		console.log(x);
+	});
+	$("#mainbuttonRemembered").click(function() {
+		$("#mainForms").attr('state', 'isLogin');
+		x = $('#mainForms .form-login');
+		inputs(x);
+	});
+	$("#mainbuttonSignup").click(function() {
+		$("#mainForms").attr('state', 'isSignup');
+		x = $('#mainForms .form-signup');
+		inputs(x);
+		console.log(x);
+	});
+	$("#mainbuttonRecover").click(function() {
+		$("#mainForms").attr('state', 'isRecover');
+		x = $('#mainForms .form-recover');
+		inputs(x);
+	});
+
+}
+function inputs(x) {
+	$('.form-input input', x).keyup(function() {
+
+		var empty = false;
+		$('.form-input input', x).each(function() {
+			if ($(this).val().length == 0) {
+				empty = true;
+			}
+		});
+
+		if (empty) {
+			$('.form-submit', x).prop('disabled', true);
+		} else {
+			$('.form-submit', x).prop('disabled', false);
+		}
 	});
 }
+
+$(document).ready(function(){
+	form();
+	inputs();
+});
