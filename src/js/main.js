2c335b7e852e2bcd32948ec4b08d3047f6a0de9d
vavilina <ann@myplaycity.com>
2017-03-30 12:03:08 +0400

    final

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,15 @@
 function filter () {
-	var filter1 = document.getElementById('Cat1'),
-		filter2 = document.getElementById('Cat2'),
+	var filter1 = document.getElementById('cat1'),
+		filter2 = document.getElementById('cat2'),
 		cat = document.getElementsByClassName('filter-items-item');
 
-	filter2.onclick = function(event) {
-		for (var i=2; i<cat.length; i+=1){
+	filter2.onclick = function () {
+		for (var i = 2; i < cat.length; i += 1){
 			cat[i].style.display = 'none';
 		}
 	}
-	filter1.onclick = function(event) {
-		for (var i=0; i<cat.length; i+=1){
+	filter1.onclick = function () {
+		for (var i = 0; i < cat.length; i += 1){
 			cat[i].style.display = 'block';
 		}
 	}
